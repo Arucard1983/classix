22a19e9ba49c05f02748c73f33a4e92eb8aa6476
Arucard1983 <nosferatu.arucard.1983@gmail.com>
2026-09-21 18:39:47 +0100

    Adding the Mixed Mode Manager
    That will simply thow a fatal exception and close the program if they dare!

---
+++ CarbonLib/MixedMode.cpp
@@ -0,0 +1,29 @@
+//
+// MixedMode.cpp
+// Classix
+//
+// Copyright (C) 2013 Félix Cloutier
+//
+// This file is part of Classix.
+//
+// Classix is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+//
+// Classix is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// Classix. If not, see http://www.gnu.org/licenses/.
+//
+
+#include "Prototypes.h"
+#include "NotSupportedException.h"
+
+void CarbonLib_NewFatRoutineDescriptor(InterfaceLib::Globals* globals, MachineState* state)
+{
+	throw PPCVM::NotSupportedException(__func__,"68k code emulation is not supported on Darling!");
+}
+
